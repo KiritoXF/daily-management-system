f75add48d929c133fc0c488d1dcd3033324af5b3
kiritoxf <yh_txyg@qq.com>
2019-09-26 17:51:35 +0800

    add tag to todo card.

--- src/views/todo/todoDetail.vue
+++ src/views/todo/todoDetail.vue
@@ -12,6 +12,40 @@
             <p>在列表 {{ listName }} 中</p>
           </Col>
         </Row>
+        <!-- tags -->
+        <Row style="margin: 10px 0">
+          <Col span="2">
+            <Icon type="md-tag"></Icon>
+          </Col>
+          <Col span="22">
+            <h2>tag</h2>
+            <Tag
+              v-for="tag in tags"
+              :key="tag.id"
+              :color="tag.color"
+              :name="tag.id"
+              :fade="false"
+              @click.native="openTagDialog"
+              style="width: 40px; height: 25px;"
+            >{{ tag.name }}</Tag>
+            <Button icon="ios-add" type="dashed" size="small" @click="openTagDialog">add tag</Button>
+            <!-- All tags modal -->
+            <Modal v-model="tagDialogShown" scrollable title="tags" width="350" :mask="false"> 
+              <p slot="header" style="text-align: center">tag</p>
+              <Tag
+                v-for="tag in allTags"
+                :key="tag.id"
+                :color="tag.color"
+                style="width: 100%"
+                :fade="false"
+                @click.native="addTag(tag.id)"
+              >
+                <Icon type="md-checkmark" v-show="tag.checked"></Icon>
+              </Tag>
+              <p slot="footer"></p>
+            </Modal>
+          </Col>
+        </Row>
         <!-- card detail -->
         <Row style="margin: 10px 0">
           <Col span="2">
@@ -78,27 +112,28 @@
               @click="closeComment"
             ></Button>
             <div v-for="comment in comments" :key="comment.id">
-              <Card style="width:320px">{{ comment.name }}</Card>
+              <Card style="width:320px">{{ comment.content }}</Card>
               <a>edit</a> -
               <a>delete</a>
             </div>
           </Col>
         </Row>
       </Col>
+      <!-- right. operate tool -->
       <Col span="6" style="padding: 0px 10px">
         <CellGroup>
           <p>add to this card</p>
-          <Cell title="Only show titles" />
-          <Cell title="Only show titles" />
-          <Cell title="Only show titles" />
-          <Cell title="Only show titles" />
-          <Cell title="Display label content" label="label content" />
+          <Cell title="person" />
+          <Cell title="tag" @click.native="openTagDialog" />
+          <Cell title="todo list" />
+          <Cell title="deadline" />
+          <Cell title="file" />
           <p>operation</p>
-          <Cell title="Only show titles" />
-          <Cell title="Only show titles" />
-          <Cell title="Only show titles" />
-          <Cell title="Only show titles" />
-          <Cell title="Only show titles" />
+          <Cell title="move"><Icon slot="icon" type="md-arrow-forward"></Icon></Cell>
+          <Cell title="copy"><Icon slot="icon" type="md-copy"></Icon></Cell>
+          <Cell title="watch"><Icon slot="icon" type="md-eye"></Icon></Cell>
+          <Cell title="delete"><Icon slot="icon" type="md-close"></Icon></Cell>
+          <Cell title="share"><Icon slot="icon" type="md-share"></Icon></Cell>
         </CellGroup>
       </Col>
     </Row>
@@ -122,11 +157,24 @@ export default {
   data() {
     return {
       textareaShown: false,
-      commentTextareaShown: "",
+      commentTextareaShown: false,
+      tagDialogShown: false,
+      tags: [],
       detail: "",
       inputDetail: "",
       comments: [],
-      inputComment: ""
+      inputComment: "",
+      allTags: [
+        { id: 1, color: "primary", checked: false },
+        { id: 2, color: "success", checked: false },
+        { id: 3, color: "error", checked: false },
+        { id: 4, color: "warning", checked: false },
+        { id: 5, color: "magenta", checked: false },
+        { id: 6, color: "volcano", checked: false },
+        { id: 7, color: "orange", checked: false },
+        { id: 8, color: "yellow", checked: false },
+        { id: 9, color: "purple", checked: false }
+      ]
     };
   },
   methods: {
@@ -155,23 +203,47 @@ export default {
       this.textareaShown = false;
       this.inputDetail = "";
     },
-    //
+    // edit comment
     editComment() {
       this.commentTextareaShown = true;
     },
-    //
+    // save comment
     saveComment() {
       if (this.inputComment.trim() === "") {
         return;
       }
       const length = this.comments.length;
-      this.comments.push({ id: length + 1, name: this.inputComment });
+      this.comments.push({ id: length + 1, content: this.inputComment });
       this.closeComment();
     },
-    //
+    // close comment-edit
     closeComment() {
       this.commentTextareaShown = false;
       this.inputComment = "";
+    },
+    // open tag dialog to edit tags
+    openTagDialog() {
+      this.tagDialogShown = true;
+    },
+    // add tag (still need to fix up sth.)
+    addTag(id) {
+      if (!this.allTags[id - 1].checked) {
+        this.tags.push(this.allTags[id - 1]);
+        this.allTags[id - 1].checked = true;
+      } else {
+        this.removeTag(null, id);
+        this.allTags[id - 1].checked = false;
+      }
+    },
+    // remove tag
+    removeTag(event, id) {
+      let index = -1;
+      this.tags.forEach((tag, i) => {
+        if (tag.id === id) {
+          index = i;
+        }
+      });
+      this.tags.splice(index, 1);
     }
   }
 };
